1ee46739198624d08d06ccb79dc2cc8a706bf950
Ben Carey <bdmc@sinemacula.co.uk>
2026-06-19 17:30:38 -0400

    feat(phpcs): add RequirePromotedPropertyComment sniff

---
+++ SineMacula/Tests/Commenting/RequirePromotedPropertyCommentSniffTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SineMacula\Tests\Commenting;
+
+use SineMacula\Tests\AbstractSniffTestCase;
+
+/**
+ * Tests for the promoted property doc comment sniff.
+ *
+ * @author      Ben Carey <bdmc@sinemacula.co.uk>
+ * @copyright   2026 Sine Macula Limited
+ */
+final class RequirePromotedPropertyCommentSniffTest extends AbstractSniffTestCase
+{
+    /**
+     * Only promoted properties without a doc comment should be flagged;
+     * documented and non-promoted parameters should not.
+     *
+     * @return void
+     */
+    public function testFlagsUndocumentedPromotedProperties(): void
+    {
+        $this->assertErrorsOnLines('RequirePromotedPropertyComment.inc', [10]);
+    }
+}
